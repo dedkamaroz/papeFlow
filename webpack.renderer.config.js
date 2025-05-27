a59c6936c5fbfa969d2152484d853b58a6a5c12e
dedkamaroz <dedkamaroz@gmail.com>
2025-05-27 10:09:49 +1000

    Update 2

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -51,17 +50,6 @@ module.exports = {
       inject: 'body',
     }),
     isDevelopment && new ReactRefreshWebpackPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'public',
-          to: '.',
-          globOptions: {
-            ignore: ['**/index.html'],
-          },
-        },
-      ],
-    }),
   ].filter(Boolean),
   devServer: {
     port: 3000,
